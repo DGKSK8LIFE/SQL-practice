45b1a049596594423ae6a09646bbde0b8cdffc86
Tarek Ali <tarekali15@outlook.com>
2019-10-15 06:34:20 -0500

    Update main.go

--- main.go
+++ main.go
@@ -4,7 +4,15 @@ import (
 	"net/http"
 	"html/template"
 	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 ) 
+
+type ShoppingCart struct {
+	gorm.Model
+	Item string
+}
+
 
 var tpl *template.Template
 
@@ -24,14 +32,26 @@ func index(w http.ResponseWriter, r *http.Request){
 }
 
 func processor(w http.ResponseWriter, r *http.Request){
+	db, err := gorm.Open("sqlite3", "storedata.db")
+	if err != nil {
+		panic("failed to connect to db")
+	}
+	defer db.Close()
 	if r.Method != "GET"{
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
 
-	item := r.FormValue("item")
+	item := r.FormValue("item")	
 
 	fmt.Println(item)
+
+	if len(db) == 0{
+		db.AutoMIgrate(&ShoppingCart{})
+		db.Create(&Product{Item: item})
+	} else {
+		db.Model(&ShoppingCart{Item: item})
+	}
 
 	tpl.ExecuteTemplate(w, "main.html", item)
 } 
